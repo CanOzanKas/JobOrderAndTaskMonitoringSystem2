7a8b82c5622e57754b33eca4a8daa736a22e6b7e
CanOzanKas <fringee01@gmail.com>
2024-09-22 19:26:43 +0300

    other objects are added

---
+++ AppCore/Entities/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Entities {
+    public class Notification {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int TaskId { get; set; }
+        public UserTask Task { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ AppCore/Entities/UserTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Entities {
+    public class UserTask {
+        public int Id { get; set; }
+        public int JobOrderId { get; set; }
+        public JobOrder JobOrder { get; set; }
+        
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int AssignedTo { get; set; }
+        public User AssignedUser { get; set; }
+
+        public DateTime DueDate { get; set; }
+        public TaskStatusEnum Status { get; set; }
+
+    }
+}
